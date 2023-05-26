4059aec26427c6632a080ecb77e401e92a70ad8d
developer-shourav <mrinmoyraj823@gmail.com>
2023-05-26 16:18:58 +0600

    category swiper added

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,11 +1,11 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
-import sliderImg1 from '../../../../assets/home/01.jpg';
-import sliderImg2 from '../../../../assets/home/02.jpg';
-import sliderImg3 from '../../../../assets/home/03.png';
-import sliderImg4 from '../../../../assets/home/04.jpg';
-import sliderImg5 from '../../../../assets/home/05.png';
-import sliderImg6 from '../../../../assets/home/06.png';
+import sliderImg1 from '../../../assets/home/01.jpg';
+import sliderImg2 from '../../../assets/home/02.jpg';
+import sliderImg3 from '../../../assets/home/03.png';
+import sliderImg4 from '../../../assets/home/04.jpg';
+import sliderImg5 from '../../../assets/home/05.png';
+import sliderImg6 from '../../../assets/home/06.png';
 const Banner = () => {
   return (
     <Carousel className="text-center" autoPlay={true} infiniteLoop={true} interval={1000}>
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
-import Banner from "./Banner/Banner";
+import Banner from "../Banner/Banner";
+import Category from "../Category/Category";
 
 
 const Home = () => {
     return (
         <div>
          <Banner> </Banner>
+         <Category> </Category>
         </div>
     );
 };
