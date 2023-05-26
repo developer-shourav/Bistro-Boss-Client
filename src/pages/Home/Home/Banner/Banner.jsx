58995160a75f5a658ed485575eda913889f221dc
developer-shourav <mrinmoyraj823@gmail.com>
2023-05-26 15:38:50 +0600

    slider added

--- src/pages/Home/Home/Banner/Banner.jsx
+++ src/pages/Home/Home/Banner/Banner.jsx
@@ -1,19 +1,37 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
+import sliderImg1 from '../../../../assets/home/01.jpg';
+import sliderImg2 from '../../../../assets/home/02.jpg';
+import sliderImg3 from '../../../../assets/home/03.png';
+import sliderImg4 from '../../../../assets/home/04.jpg';
+import sliderImg5 from '../../../../assets/home/05.png';
+import sliderImg6 from '../../../../assets/home/06.png';
 const Banner = () => {
   return (
-    <Carousel>
+    <Carousel className="text-center" autoPlay={true} infiniteLoop={true} interval={1000}>
       <div>
-        <img src="assets/1.jpeg" />
-        <p className="legend">Legend 1</p>
+        <img src={sliderImg1} />
+        
       </div>
       <div>
-        <img src="assets/2.jpeg" />
-        <p className="legend">Legend 2</p>
+        <img src={sliderImg2} />
+        
       </div>
       <div>
-        <img src="assets/3.jpeg" />
-        <p className="legend">Legend 3</p>
+        <img src={sliderImg3} />
+        
+      </div>
+      <div>
+        <img src={sliderImg4} />
+        
+      </div>
+      <div>
+        <img src={sliderImg5} />
+        
+      </div>
+      <div>
+        <img src={sliderImg6} />
+        
       </div>
     </Carousel>
   );
--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -32,7 +32,7 @@ const NavBar = () => {
     </>
   return (
     <>
-      <div className="navbar bg-base-100">
+      <div className="navbar max-w-screen-xl fixed z-10 bg-black text-white bg-opacity-30">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
