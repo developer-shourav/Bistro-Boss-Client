d89ceb42dc59561f49ae6e131efdd9d1150c8589
developer-shourav <mrinmoyraj823@gmail.com>
2023-05-28 11:35:57 +0600

    login form added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,64 @@
-
 const Login = () => {
-    return (
-        <div>
-            <h2 className="text-6xl text-blue-700 text-center font-bold py-10">Please login</h2>
+    const handleLogin = event => {
+       event.preventDefault()
+       const form = event.target;
+       const email = form.email.value;
+       const password = form.password.value;
+       console.log({email}, {password});
+       
+       form.reset()
+       
+    }
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col md:flex-row">
+       
+        <div className="card flex-shrink-0 md:w-1/2 w-full max-w-sm shadow-2xl bg-base-100">
+          <form onSubmit={handleLogin} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="text"
+                placeholder="email"
+                className="input input-bordered"
+                name="email"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+                name="password"
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <input className="btn btn-primary" type="submit" value="Login" />
+            </div>
+          </form>
         </div>
-    );
+
+        <div className="text-center md:w-1/2 lg:text-left">
+          <h1 className="text-5xl font-bold">Login now!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
