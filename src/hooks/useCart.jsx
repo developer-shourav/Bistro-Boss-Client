29f2b160da4064e73c817817f4271f27be8c2595
developer-shourav <mrinmoyraj823@gmail.com>
2023-06-02 07:30:14 +0600

    dynamic fetch hook added

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,10 +1,21 @@
 
 import { useQuery } from "@tanstack/react-query";
+
 import { useContext } from "react";
 import { AuthContext } from "../providers/AuthProviders";
 
 const useCart = () => {
   const {user} = useContext(AuthContext);
+
+  const { isLoading, refetch,  data: cart =[] } = useQuery({
+    queryKey: ['carts', user?.email],
+    queryFn: async () => {
+      const res = await fetch(`http://localhost:7000/carts?email=${user?.email}`)
+
+      return res.json();
+    },
+  })
+  return [cart, isLoading, refetch]
   
 }
 
