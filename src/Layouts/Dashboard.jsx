2c3ddb2d1681c3a0f0dc7e048261733289a0f600
developer-shourav <mrinmoyraj823@gmail.com>
2023-06-03 14:10:05 +0600

    conditional user admint dashboard routing added

--- src/Layouts/Dashboard.jsx
+++ src/Layouts/Dashboard.jsx
@@ -1,41 +1,126 @@
 import { NavLink, Outlet } from "react-router-dom";
-import { FaShoppingCart, FaWallet, FaCalendarAlt, FaHome } from "react-icons/fa";
-import {GrMenu} from 'react-icons/gr';
-import {MdLocalDining} from 'react-icons/md';
+import {
+  FaShoppingCart,
+  FaWallet,
+  FaCalendarAlt,
+  FaHome,
+  FaUtensils,
+  FaBook,
+  FaUsers,
+} from "react-icons/fa";
+import { GrMenu } from "react-icons/gr";
+import { MdLocalDining } from "react-icons/md";
+import { TfiMenuAlt } from "react-icons/tfi";
 import useCart from "../hooks/useCart";
 
 const Dashboard = () => {
   const [cart] = useCart();
+
+  // TODO : Load data from server to have dynamic isAdmin based on Data
+  const isAdmin = true;
+
   return (
     <div className="drawer lg:drawer-open">
-    <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-    <div className="drawer-content flex flex-col items-center justify-center">
+      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex flex-col items-center justify-center">
+        <Outlet> </Outlet>
+        <label
+          htmlFor="my-drawer-2"
+          className="btn btn-primary drawer-button lg:hidden"
+        >
+          Open drawer
+        </label>
+      </div>
+      <div className="drawer-side bg-[#D1A054]">
+        <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+        <ul className="menu p-4 w-80 h-full  ">
 
-    <Outlet> </Outlet>
-      <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-    
-    </div> 
-    <div className="drawer-side bg-[#D1A054]">
-      <label htmlFor="my-drawer-2" className="drawer-overlay"></label> 
-      <ul className="menu p-4 w-80 h-full  ">
-        
-        <li><NavLink to='/dashboard/home'><FaHome/> User Home</NavLink></li>
-        <li><NavLink to='/dashboard/reservations'><FaCalendarAlt/> Reservations</NavLink></li>
-        <li><NavLink to='/dashboard/history'><FaWallet/> Payment History</NavLink></li>
-        <li><NavLink className='flex' to='/dashboard/mycart'><FaShoppingCart/> My Cart<span className="badge  badge-secondary">+{cart.length || 0}</span></NavLink>
-        </li>
+{/* --------------User and Admin Based Dynamic Dashboard----------- */}
+          {isAdmin ? (
+            <>
+              {" "}
+              <li>
+                <NavLink to="/dashboard/home">
+                  <FaHome /> Admin Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/reservations">
+                  <FaUtensils /> Add Items
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/history">
+                  <TfiMenuAlt /> Manage Items
+                </NavLink>
+              </li>
 
-        <div className="divider"></div>
+              <li>
+                <NavLink to="/dashboard/history">
+                  <FaBook /> Manage Bookings
+                </NavLink>
+              </li>
 
-        <li><NavLink to='/'><FaHome/>Home</NavLink></li>
-        <li><NavLink to='/menu'><GrMenu/>  Our Menu</NavLink></li>
-      <li><NavLink to='/order/salad'><MdLocalDining/> Order Food</NavLink></li>
+              <li>
+                <NavLink to="/dashboard/allusers">
+                  <FaUsers /> All Users
+                </NavLink>
+              </li>
+              
+            </>
+          ) : (
+            <>
+              {" "}
+              <li>
+                <NavLink to="/dashboard/home">
+                  <FaHome /> User Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/reservations">
+                  <FaCalendarAlt /> Reservations
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/history">
+                  <FaWallet /> Payment History
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="flex" to="/dashboard/mycart">
+                  <FaShoppingCart /> My Cart
+                  <span className="badge  badge-secondary">
+                    +{cart.length || 0}
+                  </span>
+                </NavLink>
+              </li>{" "}
+            </>
+          )}
 
-        
-      </ul>
-    
+
+
+{/* ------------Divider And Common Routes for both Users and Admin------------ */}
+          <div className="divider"></div>
+
+          <li>
+            <NavLink to="/">
+              <FaHome />
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/menu">
+              <GrMenu /> Our Menu
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/order/salad">
+              <MdLocalDining /> Order Food
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
   );
 };
 
